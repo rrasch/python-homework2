c628bcdbe5fe8189bcdaf22677ec09af2ab8cfdb
Rasan Rasch <rasan@nyu.edu>
2022-09-27 22:25:09 -0400

    Add homework 2.3

---
+++ homework_2.3.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+
+"""
+    homework_2.2.py -- creates a new table called weather_newyork
+                       in the session_2.db
+
+    Author: Rasan Rasch (rasan@nyu.edu)
+    Last Revised: 9/27/2022
+"""
+
+import csv
+import os
+import sqlite3
+
+
+def create_and_fill_weather_ny_table(dbname, csvfile):
+    conn = sqlite3.connect(dbname)
+    cur = conn.cursor()
+
+    cur.execute("DROP TABLE IF EXISTS weather_newyork")
+    conn.commit()
+
+    cur.execute(
+        """
+        CREATE TABLE weather_newyork (
+            date TEXT,
+            mean_temp INT,
+            precip FLOAT,
+            events TEXT
+        )
+"""
+    )
+    conn.commit()
+
+    query = "INSERT INTO weather_newyork VALUES (?, ?, ?, ?)"
+
+    fh = open(csv_file)
+    reader = csv.reader(fh)
+    headers = next(reader)
+
+    for fields in reader:
+        date = fields[0]
+        mean_temp = fields[1]
+        precip = fields[-4]
+        events = fields[-2]
+
+        if precip == "T":
+            precip = None
+
+        cur.execute(query, [date, mean_temp, precip, events])
+        conn.commit()
+
+    fh.close()
+
+    conn.close()
+
+
+data_dir = os.path.join(
+    os.environ["HOME"], "python_data_apy", "session_02_working_files"
+)
+db_name = os.path.join(data_dir, "session_2.db")
+csv_file = os.path.join(data_dir, "weather_newyork.csv")
+
+
+create_and_fill_weather_ny_table(db_name, csv_file)
+
