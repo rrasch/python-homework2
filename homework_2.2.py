e188f3ef7ed4df69354f04580d61622251977392
Rasan Rasch <rasan@nyu.edu>
2022-09-27 21:30:28 -0400

    Add homework 2.2

---
+++ homework_2.2.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+"""
+    homework_2.2.py -- creates a new table called weather_newyork
+                       in the session_2.db
+
+    Author: Rasan Rasch (rasan@nyu.edu)
+    Last Revised: 9/27/2022
+"""
+
+import os
+import sqlite3
+
+def create_weather_ny_table(dbname):
+    conn = sqlite3.connect(dbname)
+    cur = conn.cursor()
+
+    cur.execute("DROP TABLE IF EXISTS weather_newyork")
+    conn.commit()
+
+    cur.execute(
+        """
+        CREATE TABLE weather_newyork (
+            date TEXT,
+            mean_temp INT,
+            precip FLOAT,
+            events TEXT
+        )
+"""
+    )
+    conn.commit()
+
+    conn.close()
+
+
+db_name = os.path.join(
+    os.environ["HOME"],
+    "python_data_apy",
+    "session_02_working_files",
+    "session_2.db",
+)
+
+create_weather_ny_table(db_name)
+
