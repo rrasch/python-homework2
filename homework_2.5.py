f8446de54fe5162d21929d74c9c78704e7115072
Rasan Rasch <rasan@nyu.edu>
2022-09-28 00:00:19 -0400

    Add homework 2.5

---
+++ homework_2.5.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+"""
+    homework_2.5.py -- Build a JSON dict of dicts based on the revenue table
+
+    Author: Rasan Rasch (rasan@nyu.edu)
+    Last Revised: 9/27/2022
+"""
+
+import json
+import os
+import pprint
+import sqlite3
+
+
+def convert_db_to_json(dbname, jsonfile):
+    conn = sqlite3.connect(dbname)
+    cur = conn.cursor()
+
+    cur.execute("SELECT * from revenue")
+
+    data = {}
+
+    for row in cur:
+        company, state, cost = row
+        print([company, state, cost])
+
+        data[company] = {"name": company, "state": state, "amount": cost}
+
+    conn.close()
+
+    fh = open(jsonfile, "w")
+    json.dump(data, fh, indent=4)
+    fh.close()
+
+
+data_dir = os.path.join(
+    os.environ["HOME"], "python_data_apy", "session_02_working_files"
+)
+db_name = os.path.join(data_dir, "session_2.db")
+json_file = "revenue.json"
+
+convert_db_to_json(db_name, json_file)
+
